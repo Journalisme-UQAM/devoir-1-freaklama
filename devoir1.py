9bb0c72d6b67c412a53634d4319498ac0e3598c6
freaklama <thomas.litalien@gmail.com>
2017-02-12 19:42:37 -0500

    Update devoir1.py

--- devoir1.py
+++ devoir1.py
@@ -1,2 +1,9 @@
 
 /devoir1.py
+salut = list(range(30000,100000))
+print(salut)
+bonjour = list(range(00000,18000))
+for num in bonjour:
+    print("{:05d}".format(num))
+    
+    Pas le format la plus optimal, je ne comprends pas comment les printer ensemble.
